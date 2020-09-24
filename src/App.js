3de4fbeea78f4ff11c3188befe0b3802e2dc0c26
cagin <nebu.gomm@gmail.com>
2020-09-24 18:40:56 +0200

    added floating window context

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import './App.css';
-import Space from './components/Space';
 import FloatingRoomWindow from './components/FloatingRoomWindow';
 import RootContextProvider from './contexts/RootContext';
 
-import './stars.css';
-
-import StarfieldAnimation from 'react-starfield-animation';
 import TravisoGameWrapper from './traviso/TravisoGameWrapper';
-
-
-
-
-
-
 
 function App() {
 	return (
@@ -32,36 +22,13 @@ function App() {
 				}}
 			/> */}
 
-			<TravisoGameWrapper />
+			<RootContextProvider>
+				<TravisoGameWrapper />
+				<FloatingRoomWindow />
+			</RootContextProvider>
 		</div>
 	);
 }
 
-
-
-
-// function App() {
-// 	return (
-// 		<div className='App'>
-// 			<div className='stars'></div>
-// 			<div className='twinkling'></div>
-
-// 			<StarfieldAnimation
-// 				numParticles={300}
-// 				lineWidth={2.0}
-// 				depth={300}
-// 				style={{
-// 					position: 'absolute',
-// 					width: '100%',
-// 					height: '100%',
-// 				}}
-// 			/>
-// 			<RootContextProvider>
-// 				<Space />
-// 				<FloatingRoomWindow />
-// 			</RootContextProvider>
-// 		</div>
-// 	);
-// }
 
 export default App;
--- src/traviso/TravisoGameWrapper.jsx
+++ src/traviso/TravisoGameWrapper.jsx
@@ -1,5 +1,6 @@
-import  React, { useRef, useEffect, useReducer } from 'react';
-import { createFloatingRoomWindow } from '../components/FloatingRoomWindow';
+import  React, { useRef, useEffect, useReducer, useContext } from 'react';
+import { FloatingSpaceContext } from '../contexts/FloatingSpaceContext';
+// import { createFloatingRoomWindow } from '../components/FloatingRoomWindow';
 import { initWhateverse } from './game';
 
 const houseActions = {
@@ -24,15 +25,25 @@ function houseReducer(state, action) {
     }
 }
 
+
+
+
+
 function TravisoGameWrapper({ }) {
     const [houses, dispatch] = useReducer(houseReducer, {});
+    const { currentFloatingSpaces, addFloatingSpace } = useContext(FloatingSpaceContext);
+
     const ref = useRef(null);
 
     const canVisit = house => house.name && !houses[house.name];
-    const onHouseVisit = house => canVisit(house) && dispatch({ type: houseActions.visit, payload: { house } });
-    const closeHouseWindow = name =>  dispatch({ type: houseActions.windowClose, payload: { name } });
+    // const onHouseVisit = house => canVisit(house) && dispatch({ type: houseActions.visit, payload: { house } });
+    const onHouseVisit = house => addFloatingSpace(house.name);
+    console.log(currentFloatingSpaces);
+    // const closeHouseWindow = name =>  dispatch({ type: houseActions.windowClose, payload: { name } });
 
     useEffect(_ => ref && initWhateverse({ onHouseVisit }, ref), [ref]);
+
+
 
     const windows = Object.keys(houses)
         .filter(key => houses[key]);
