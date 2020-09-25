e255f8adcdf41088130ef040a8230b789d88468b
cagin <nebu.gomm@gmail.com>
2020-09-25 18:52:44 +0200

    Removed unnecessary comments

--- src/contexts/FloatingSpaceContext.js
+++ src/contexts/FloatingSpaceContext.js
@@ -13,23 +13,13 @@ const FloatingSpaceContextProvider = props => {
       RoomNames.indexOf(windowKey) !== -1 &&
       currentFloatingSpaces.findIndex(s => RoomNames.indexOf(s) !== -1) !== -1
     ) {
-      // console.warm('#1 if is true')
-      // console.warn(RoomNames.indexOf(windowKey) !== -1)
-      // console.warn(currentFloatingSpaces.findIndex(s => RoomNames.indexOf(s) !== -1) !== -1);
-      // console.warn('before:', currentFloatingSpaces);
       const spaces = currentFloatingSpaces
           .filter(s => RoomNames.indexOf(s) === -1)
           .concat([windowKey])
 
-      // console.warn('after', spaces);
       setFloatingSpaces(spaces)
     } else {
-      // console.warn('#2 if is not true')
-      // console.warn(RoomNames.indexOf(windowKey) !== -1)
-      // console.warn(currentFloatingSpaces.findIndex(s => RoomNames.indexOf(s) !== -1) !== -1);
-      // console.warn('before:', currentFloatingSpaces);
       const spaces = currentFloatingSpaces.concat([windowKey]);
-      // console.warn('after', spaces);
       setFloatingSpaces(spaces);
     }
   }
